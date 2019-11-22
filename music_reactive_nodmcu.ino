7229715f5a7c707d37db0f5fedecc31717f9d99d
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-11-23 02:29:40 +0530

    Color picker done

--- music_reactive_nodmcu.ino
+++ music_reactive_nodmcu.ino
@@ -1,9 +1,34 @@
+#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#define FASTLED_ALLOW_INTERRUPTS 0
+
 #include <FastLED.h>
+#include <ESP8266WiFi.h>
 
 #define NUM_LEDS 300
 #define LED_PIN 3
-#define SOUND_PIN 1
+#define SOUND_PIN 0
 #define brightness 255
+
+const char *ssid = "silentlad";
+const char *password = "Silentlad1!";
+
+WiFiServer server(1234);
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+// Decode HTTP GET value
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
 
 const int sampleWindow = 10; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
@@ -55,6 +80,19 @@ CRGB leds[NUM_LEDS];
 void setup()
 {
     Serial.begin(9600);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
     // put your setup code here, to run once:
     FastLED.addLeds<WS2812B, LED_PIN, BRG>(leds, NUM_LEDS);
     FastLED.setBrightness(brightness);
@@ -68,6 +106,86 @@ void setup()
 
 void loop()
 {
+    WiFiClient client = server.available(); // Listen for incoming clients
+
+    if (client)
+    { // If a new client connects,
+        currentTime = millis();
+        previousTime = currentTime;
+        Serial.println("New Client."); // print a message out in the serial port
+        String currentLine = "";       // make a String to hold incoming data from the client
+        while (client.connected() && currentTime - previousTime <= timeoutTime)
+        { // loop while the client's connected
+            currentTime = millis();
+            if (client.available())
+            {                           // if there's bytes to read from the client,
+                char c = client.read(); // read a byte, then
+                Serial.write(c);        // print it out the serial monitor
+                header += c;
+                if (c == '\n')
+                { // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0)
+                    {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
+                        client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
+                        client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
+                        client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
+                        client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
+                        client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+                        client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
+                        // The HTTP response ends with another blank line
+                        client.println();
+
+                        // Request sample: /?r201g32b255&
+                        // Red = 201 | Green = 32 | Blue = 255
+                        if (header.indexOf("GET /?r") >= 0)
+                        {
+                            pos1 = header.indexOf('r');
+                            pos2 = header.indexOf('g');
+                            pos3 = header.indexOf('b');
+                            pos4 = header.indexOf('&');
+                            redString = header.substring(pos1 + 1, pos2);
+                            greenString = header.substring(pos2 + 1, pos3);
+                            blueString = header.substring(pos3 + 1, pos4);
+                            Serial.println(redString.toInt());
+                            Serial.println(greenString.toInt());
+                            Serial.println(blueString.toInt());
+                        }
+                        // Break out of the while loop
+                        break;
+                    }
+                    else
+                    { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+                }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
+    }
+
     unsigned long startMillis = millis(); // Start of sample window
     unsigned int peakToPeak = 0;          // peak-to-peak level
 
@@ -77,7 +195,7 @@ void loop()
     // collect data for 50 mS
     while (millis() - startMillis < sampleWindow)
     {
-        sample = analogRead(1);
+        sample = analogRead(SOUND_PIN);
         if (sample < 1024) // toss out spurious readings
         {
             if (sample > signalMax)
@@ -103,11 +221,15 @@ void loop()
                                  //  Serial.print(" ");
     Serial.println(curshow);
     //  FillLEDsFromPaletteColors( startIndex,curshow,0);
-    FillLEDsFromPaletteColors(startIndex, curshow, 1);
+    // FillLEDsFromPaletteColors(startIndex, curshow, 1);
     //  FillLEDsFromPaletteColors( startIndex,curshow,2);
     //  FillLEDsFromPaletteColors( startIndex,curshow,4);
     //  RunBeats(peakToPeak);
     //  RunBeats2(peakToPeak);
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CRGB(redString.toInt(), greenString.toInt(), blueString.toInt());
+    }
     FastLED.show();
     loopCount++;
     //  delay(50);
